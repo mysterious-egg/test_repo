23726daf1379aa0be7fe522e8c182a799eb5149d
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2024-02-19 23:44:53 +0530

    Ading Server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.json({"messgae":'pong'});
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
